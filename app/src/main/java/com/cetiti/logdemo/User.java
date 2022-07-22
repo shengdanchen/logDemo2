76ce03c8e4b2c0a91d07499277a0893e44dfca70
Csd <515593477@qq.com>
2022-07-22 19:46:13 +0800

    222

---
+++ app/src/main/java/com/cetiti/logdemo/User.java
@@ -0,0 +1,18 @@
+package com.cetiti.logdemo;
+
+/**
+ * author : ChenShengDan
+ * date   : 2022/7/22
+ * desc   :
+ */
+public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
